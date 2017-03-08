37a7bf1feed19377b03d1013c7ee8ca6be631094
Anastassios Martakos <amartako@phys.ethz.ch>
2017-03-08 09:52:09 +0100

    Added docstring to PluginBase and implemented its __specifize_regex_group_name method

--- lib/plugin_base.py
+++ lib/plugin_base.py
@@ -1,27 +1,38 @@
+"""Here are the base classes of every Plugin."""
+
 from .exceptions import NoMatch
 import abc
 
 
 class IPlugin(metaclass=abc.ABCMeta):
+    """Interface which every plugin has to implement."""
+
     @abc.astractmethod
     def _check_subscription(self, line):
+        """Return True or False if the subscription regex matched or not."""
         pass
 
     @abc.abstractmethod
     def _define_subscription_regex(self):
+        """Assign the compiled subscription regex to self."""
         pass
 
     @abc.abstractmethod
     def _define_data_regex(self):
+        """Assign the compiled data regex to self."""
         pass
 
     @abc.abstractmethod
     def gather_data(self, line):
+        """Extract the data from a logline and return the results as a dict."""
         pass
 
 
 class PluginBase(IPlugin):
+    """Base class of every plugin, which contains generalized logic."""
+
     def __init__(self):
+        """Ctor of PluginBase."""
         self._subscriptionRegex = None
         self._define_subscription_regex()
 
@@ -32,19 +43,39 @@ class PluginBase(IPlugin):
         self.__exract_regex_group_names()
 
     def __specifize_regex_group_name(self, regexMatches):
-        pass
+        """
+        Replace special keywords in the regex match group.
+
+        Those special keywords are:
+        hostname    : .group('hostname')
+        servicename : __class__.__name__
+        """
+
+        newName = None
+        for name, value in regexMatches:
+            separateNames = name.split('_')
+            hostname = regexMatches.get('hostname')
+            servicename = self.__class__.__name__.lower()
+
+            if hostname is not None:
+                newName = name.replace('hostname', hostname).replace('servicename', servicename)
+
+            if newName is not None:
+                regexMatches[newName] = regexMatches.pop(name)
+
 
     def __extract_regex_group_names(self):
+        """Extract the group names of a regex."""
         pattern = self.__dataRegex.pattern
         lastpos = 0
 
         while(True):
-            pos = pattern.find('?P<', lastpos)
-            if pos != -1:
-                pos += 3
-                closingPos = pattern.find('>', pos)
-                self.__regexGroupNames.append(pattern[pos:closingPos])
-                lastpos = closingPos
+            startPos = pattern.find('?P<', lastpos)
+            if startPos != -1:
+                startPos += 3
+                endPos = pattern.find('>', startPos)
+                self.__regexGroupNames.append(pattern[startPos:endPos])
+                lastpos = endPos
             else:
                 break
 
