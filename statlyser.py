bc751a72e727865105089b9364552ec798d9d4b7
Anastassios Martakos <amartako@phys.ethz.ch>
2017-03-08 08:51:59 +0100

    Implemented main

---
+++ statlyser.py
@@ -0,0 +1,37 @@
+"""
+Statlyser. Generate statistics out of information from logfiles.
+
+This is the main (entry point) of the application.
+"""
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from lib.plugin_manager import PluginManager
+
+
+def main():
+    """Entry point of the application."""
+    pluginManager = PluginManager()
+    if len(sys.argv) > 2:
+        print('ERROR:')
+        print('First parameter has to be the logfile.')
+        print('Currently only one logfile per run is supported.')
+
+    logfile = sys.argv[1]
+
+    with open(logfile, 'r') as f:
+        line = f.readline()
+
+        while line:
+            pluginManager.process_line()
+            line = f.readline()
+
+    # output the statistic to STDOUT
+    pluginManager.statistic.represent()
+
+
+"""We only start with the executation if we are the main."""
+if __name__ == '__main__':
+    main()
