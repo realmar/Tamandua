1181e547cf3fad65ff14c236b87375c486ca2d70
Anastassios Martakos <amartako@phys.ethz.ch>
2017-03-08 12:59:51 +0100

    dovecot plugin filename to lowercase ; added amavis plugin

---
+++ Plugins/amavis.py
@@ -0,0 +1,14 @@
+"""Amavis statistics plugin."""
+
+from lib.plugin_base import PluginBase
+import re
+
+
+class Dovecot(PluginBase):
+    """Amavis statistics plugin."""
+
+    def _define_subscription_regex(self):
+        self._subscriptionRegex = re.compile(' amavis\[')
+
+    def _define_data_regex(self):
+        self._dataRegex = re.compile('\/(?P<hostname>[^\s]*) amavis\[[^\]]*\]: \([^\)]*\) (?P<servicename_virus>[^ ]* [^ ]*)')
