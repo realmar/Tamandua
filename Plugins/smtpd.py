edf085fae8d7a54eed2bd56e4f6459de8331a73f
Anastassios Martakos <amartako@phys.ethz.ch>
2017-03-27 14:32:30 +0200

    catch auth with no sasl

--- Plugins/smtpd.py
+++ Plugins/smtpd.py
@@ -14,8 +14,24 @@ class Smtpd(PluginBase):
         self._dataRegex = [
             re.compile(r'(?P<servicename_hostname_connection>(dis)?connect )'),
             re.compile(r'''
-                    sasl_method=(?P<servicename_hostname_saslmethod>[^,]*),\s
+                client=(?P<servicename_hostname_client>[^\[]*)
+                \[
+                (?P<servicename_hostname_clientip>[^\]]*)
+                (
+                    \],\s
+                    sasl_method=(?P<servicename_hostname_saslmethod>[^,]*)
+                    ,\s
                     sasl_username=(?P<servicename_hostname_saslauth_BOOL>[^\n]*)
-                    ''', re.X),
+                )?
+                ''', re.X),
             re.compile(r'(?P<servicename_hostname_action>hold|reject):'),
         ]
+
+    def _edit_results(self, results):
+        # currently we dont want the client in our statistic
+
+        try:
+            del results['servicename_hostname_client']
+            del results['servicename_hostname_clientip']
+        except Exception as e:
+            pass
