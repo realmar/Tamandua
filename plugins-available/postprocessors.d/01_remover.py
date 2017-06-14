3e0b34fe13c91eed7c459b856f5bf9752b6ef757
Anastassios Martakos <amartako@phys.ethz.ch>
2017-06-14 15:35:19 +0200

    parser: pp: remove "user unknown" rejected mails

--- plugins-available/postprocessors.d/01_remover.py
+++ plugins-available/postprocessors.d/01_remover.py
@@ -6,7 +6,19 @@ from lib.plugins.plugin_helpers import check_value
 
 
 class RemoveUnwanted(IProcessorPlugin):
+    __removeRejectReason = [
+        'user unknown',
+        'account expired'
+    ]
+
     def process(self, obj: ProcessorData) -> None:
+        rejectreason = obj.data.get('rejectreason')
+
+        for reason in self.__removeRejectReason:
+            if check_value(rejectreason, lambda x: reason in x.lower()):
+                obj.action = ProcessorAction.DELETE
+                return
+
         recipient = obj.data.get('recipient')
         if check_value(recipient, lambda x: x.lower() == 'emailtest@phys.ethz.ch'):
             obj.action = ProcessorAction.DELETE
@@ -14,10 +26,5 @@ class RemoveUnwanted(IProcessorPlugin):
 
         sender = obj.data.get('sender')
         if check_value(sender, lambda x: x.lower() == 'isg+emailtest@phys.ethz.ch'):
-            obj.action = ProcessorAction.DELETE
-            return
-
-        rejectreason = obj.data.get('rejectreason')
-        if check_value(rejectreason, lambda x: 'account expired' in x.lower()):
             obj.action = ProcessorAction.DELETE
             return
