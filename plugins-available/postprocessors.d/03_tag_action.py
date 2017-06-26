89fdbdaeba7de6f92bb2f06d2127bbde55e63575
Anastassios Martakos <amartako@phys.ethz.ch>
2017-06-26 10:38:18 +0200

    parser: pp: use only one tag for rejected mails

--- plugins-available/postprocessors.d/03_tag_action.py
+++ plugins-available/postprocessors.d/03_tag_action.py
@@ -8,23 +8,23 @@ from src.plugins.plugin_helpers import add_tag, check_value, is_rejected
 class TagAction(IProcessorPlugin):
     """Postprocessing Plugin which adds tag depending if the mail was rejected, hold, etc."""
 
-    __rejectReasons = {
-        'no_spam_wanted'        : 'no spam wanted',
-        'malformed_dns'         : 'malformed dns server reply',
-        'domain_not_found'      : 'domain not found',
-        'died'                  : 'has passed away',
-        'user_unknown'          : 'user unknown',
-        'does_not_receive_mail' : 'user does not receive mail on this account anymore',
-        'cannot_receive_mail'   : 'account cannot receive e-mail',
-        'helo_rejected'         : 'helo command rejected',
-        'no_phishing_wanted'    : 'no phishing wanted',
-        'malware'               : 'no mail from malware',
-        'no_longer_at_dphys'    : 'no longer at eth zurich d-phys',
-        'greylisted'            : 'greylisted',
-        'relay_denied'          : 'relay access denied',
-        'encryption_required'   : 'session encryption is required',
-        'client_rejected'       : 'client host rejected'
-    }
+    __rejectReasons = [
+        'no spam wanted',
+        'malformed dns server reply',
+        'domain not found',
+        'has passed away',
+        'user unknown',
+        'user does not receive mail on this account anymore',
+        'account cannot receive e-mail',
+        'helo command rejected',
+        'no phishing wanted',
+        'no mail from malware',
+        'no longer at eth zurich d-phys',
+        'greylisted',
+        'relay access denied',
+        'session encryption is required',
+        'client host rejected'
+    ]
 
     def process(self, obj: ProcessorData) -> None:
         action = obj.data.get("action")
@@ -34,7 +34,7 @@ class TagAction(IProcessorPlugin):
         elif is_rejected(obj.data):
             reason = obj.data.get('rejectreason')
 
-            for tag, s in self.__rejectReasons.items():
-                if check_value(reason, lambda x: s.lower() in x.lower()):
-                    add_tag(obj.data, tag)
+            for r in self.__rejectReasons:
+                if check_value(reason, lambda x: r.lower() in x.lower()):
+                    add_tag(obj.data, 'reject')
                     break
