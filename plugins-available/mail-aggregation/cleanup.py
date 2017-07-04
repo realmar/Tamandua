75bd842d0718043d31340a63ed0620e70baafc37
Anastassios Martakos <amartako@phys.ethz.ch>
2017-07-04 09:55:20 +0200

    parser: plugin: cleanup: also catch 'resent-message-id'

--- plugins-available/mail-aggregation/cleanup.py
+++ plugins-available/mail-aggregation/cleanup.py
@@ -14,6 +14,6 @@ class Cleanup(SimplePlugin):
 
     def _define_data_regex(self):
         self._dataRegex = [
-            re.compile(r''':\s(?P<''' + constants.HOSTNAME_QID + r'''>[^:]*):\s
+            re.compile(r''':\s(?P<''' + constants.HOSTNAME_QID + r'''>[^:]*):.+?
                            message-id=<(?P<''' + constants.MESSAGEID + r'''>[^>]*)''', re.X)
         ]
