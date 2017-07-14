6be2f062b6343d3a8d3fe9e2a58a4c0510b44679
Anastassios Martakos <amartako@phys.ethz.ch>
2017-07-14 09:48:52 +0200

    parser: pp: correctly tag rejected mails

--- plugins-available/postprocessors.d/03_tag_action.py
+++ plugins-available/postprocessors.d/03_tag_action.py
@@ -8,6 +8,7 @@ from src.plugins.plugin_helpers import add_tag, check_value, is_rejected
 class TagAction(IProcessorPlugin):
     """Postprocessing Plugin which adds tag depending if the mail was rejected, hold, etc."""
 
+    """
     __rejectReasons = [
         'no spam wanted',
         'malformed dns server reply',
@@ -25,6 +26,7 @@ class TagAction(IProcessorPlugin):
         'session encryption is required',
         'client host rejected'
     ]
+    """
 
     def process(self, obj: ProcessorData) -> None:
         action = obj.data.get("action")
@@ -32,9 +34,4 @@ class TagAction(IProcessorPlugin):
         if action == 'hold':
             add_tag(obj.data, 'hold')
         elif is_rejected(obj.data):
-            reason = obj.data.get('rejectreason')
-
-            for r in self.__rejectReasons:
-                if check_value(reason, lambda x: r.lower() in x.lower()):
-                    add_tag(obj.data, 'reject')
-                    break
+            add_tag(obj.data, 'reject')
